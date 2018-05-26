24892b822c09277235c2dc5ddfd9eaeb5b4e1b40
hannsen <jan@hro-home.de>
2018-05-26 17:20:43 +0200

    jackett fixup, closes #5

--- jackett.py
+++ jackett.py
@@ -1,7 +1,13 @@
-#VERSION: 0.1
+#VERSION: 1.0
 #AUTHORS: ukharley
+#         hannsen (github.com/hannsen)
 #
-#
+#         Make sure to change your api key below
+
+user_data = {
+    'url': 'http://127.0.0.1:9117',  # default, change to yours if different
+    'api_key': 'YOUR_API_KEY_HERE',  # add your api key
+}
 
 from novaprinter import prettyPrinter
 from helpers import retrieve_url, download_file
@@ -15,77 +21,52 @@ except ImportError:
     from urllib.parse import urlencode, quote, unquote
 
 
-class jacket(object):
+class jackett(object):
     """Generic provider for Torznab compatible api."""
 
-    def __init__(self):
-        pass
-
-    name = 'Jacket(torznab)'
-    url = 'http://127.0.0.1:9117'  # default, change to yours
-    api_key = ''  # add your api key
-    supported_categories = {'all': '',
-                            'movies': '2000',
-                            'tv': '5000',
-                            'music': '3000',
-                            'books': '8000'
-                            }
+    name = 'Jackett(torznab)'
+    url = user_data['url']
+    api_key = user_data['api_key']
+    supported_categories = {
+        'all': '',
+        'movies': '2000',
+        'tv': '5000',
+        'music': '3000',
+        'books': '8000'
+    }
 
     def search(self, what, cat='all'):
         cat = cat.lower()
-        baseURL = "http://127.0.0.1:9117/api/v2.0/indexers/all/results?%s"
-        what = unquote(what)
+        base_url = self.url + "/api/v2.0/indexers/all/results?%s"
         category = self.supported_categories[cat]
-        if category != '':
-            params = urlencode({
-                'apikey': self.api_key,
-                'Query': what,
-                'Category[]': category
-            })
-        else:
-            params = urlencode({
-                'apikey': self.api_key,
-                'Query': what,
-            })
 
-        response = retrieve_url(baseURL % params)
+        params = {
+            'apikey': self.api_key,
+            'Query': what
+        }
+        if category != '':
+            params['Category[]'] = category
+        params = urlencode(params)
+
+        response = retrieve_url(base_url % params)
         j = json.loads(response)
         for i in j['Results']:
-            tbytes = float(i['Size'])
-            size = "-1"
-
-            if tbytes > 1024 * 1024 * 1024:
-                size = "%.1f GB" % (tbytes / (1024 * 1024 * 1024))
-
-            elif tbytes > 1024 * 1024:
-                size = "%.1f MB" % (tbytes / (1024 * 1024))
-
-            elif tbytes > 1024:
-                size = "%.1f KB" % (tbytes / 1024)
-
-            else:
-                size = "%.1f B" % (tbytes)
+            res = dict(
+                name=i['Title'],
+                size='%d B' % i['Size'],
+                seeds=i['Seeders'],
+                leech=i['Peers'],
+                engine_url=self.url,
+                desc_link=i['Comments'])
 
             if i['MagnetUri']:
-                res = dict(link=i['MagnetUri'],
-                           name=i['Title'],
-                           size=size,
-                           seeds=i['Seeders'],
-                           leech=i['Peers'],
-                           engine_url=self.url,
-                           desc_link=i['Comments'])
+                res['link'] = i['MagnetUri']
             else:
-                res = dict(link=i['Link'],
-                           name=i['Title'],
-                           size=size,
-                           seeds=i['Seeders'],
-                           leech=i['Peers'],
-                           engine_url=self.url,
-                           desc_link=i['Comments'])
+                res['link'] = i['Link']
 
             prettyPrinter(res)
 
 
 if __name__ == "__main__":
-    s = jacket()
+    s = jackett()
     s.search("harry potter", 'movies')
