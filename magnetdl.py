3995ae8b8b23c9f13798819161c679842215d058
hannsen <jan@hro-home.de>
2018-07-22 13:58:26 +0200

    Update magnetdl.py

--- magnetdl.py
+++ magnetdl.py
@@ -1,7 +1,7 @@
-#VERSION: 1.2
-#AUTHORS: hoanns
+#VERSION: 1.3
+#AUTHORS: hoanns,nindogo
 # magnetdl.com
-# only first page atm
+# first thirty pages
 
 import re
 # qBt
@@ -14,26 +14,34 @@ class magnetdl(object):
     url = "http://www.magnetdl.com/"
     name = "MagnetDL"
     result_page_match = re.compile(
-        '<td\sclass="m"><a\shref="(magnet.*?)"\stitle=".*?class="n"><a\shref="(.*?)"\stitle="(.*?)">.*?<td\sclass="t.">.*?</td><td>.*?</td><td>(.*?)</td><td\sclass="s">(.*?)</td><td\sclass="l">(.*?)</td>')
+        r'<td\sclass="m"><a\shref="(magnet.*?)"\stitle=".*?class="n"><a\shref="(.*?)"\stitle="(.*?)">.*?<td\sclass="t.">.*?</td><td>.*?</td><td>(.*?)</td><td\sclass="s">(.*?)</td><td\sclass="l">(.*?)</td>')
+    total_results_num = re.compile(
+        r'<div id="footer">Found <strong>(.*)<\/strong> Magnet Links for <i>')
 
     def search(self, what, cat='all'):
         what = what.lower()
-        query = self.url + what[:1] + '/' + what
-        print(query)
-        data = retrieve_url(query)
-        results = re.findall(self.result_page_match, data)
+        running_total, total_results, pages = 0, 1, 0
 
-        for result in results:
-            temp_result = {
-                'name': result[2],
-                'size': result[3],
-                'link': result[0],
-                'desc_link': self.url[:-1] + result[1],
-                'seeds': result[4],
-                'leech': result[5],
-                'engine_url': self.url
-            }
-            prettyPrinter(temp_result)
+        while running_total < total_results and pages <= 29:
+            pages += 1
+            query = self.url + what[:1] + '/' + what +'/' + str(pages)
+            # print(query)
+            data = retrieve_url(query)
+            total_results = int(re.findall(self.total_results_num, data)[0].replace(',', ''))
+            results = re.findall(self.result_page_match, data)
+
+            for result in results:
+                temp_result = {
+                    'name': result[2].replace('|', '') ,
+                    'size': result[3].replace(',', '') ,
+                    'link': result[0],
+                    'desc_link': self.url[:-1] + result[1],
+                    'seeds': result[4],
+                    'leech': result[5],
+                    'engine_url': self.url
+                }
+                prettyPrinter(temp_result)
+                running_total += 1
 
         return
 
