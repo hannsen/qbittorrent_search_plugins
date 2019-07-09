fa12f8caadc4ce38f4126531cdaf03b4a6fc7596
jan.karberg <jan.karberg@quodata.de>
2019-07-09 21:02:09 +0200

    make grep case insensitive

--- tests/all.sh
+++ tests/all.sh
@@ -5,13 +5,13 @@ set -e
 cd /app
 
 echo TEST ali213
-python ali213.py | grep -e ark -e survival
+python ali213.py | grep -i -e ark -e survival
 
 #offline
 #python demonoid.py | grep drive
 
 echo TEST smallgames
-python smallgames.py | grep ECO
+python smallgames.py | grep -i ECO
 
 #offline
 #python threedm.py | grep Handball
